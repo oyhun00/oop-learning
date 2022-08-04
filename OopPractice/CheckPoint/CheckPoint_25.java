bf4be93e64506a9fe32e0cb9a06f3f957298200b
obeybook <obeybook@gmail.com>
2022-08-05 00:01:58 +0900

    learning oop basic

---
+++ OopPractice/CheckPoint/CheckPoint_25.java
@@ -0,0 +1,10 @@
+package OopPractice.CheckPoint;
+
+public class CheckPoint_25 {
+  private String id;
+  private String name;
+  private Integer numOfStudents = 0;
+
+  public void addStudent() {};
+  public void deleteStudent() {};
+}
---
+++ OopPractice/CheckPoint/CheckPoint_35.java
@@ -0,0 +1,27 @@
+package OopPractice.CheckPoint;
+
+public class CheckPoint_35 {
+  public abstract class HomeAppliancies {
+    private Integer serialNo;
+    private String manufacturer;
+    private Integer year;
+
+    public abstract void turnOn();
+    public abstract void turnOff();
+  }
+
+  public class Washer extends HomeAppliancies {
+    public void turnOn() {};
+    public void turnOff() {};
+  }
+
+  public class Freezer extends HomeAppliancies {
+    public void turnOn() {};
+    public void turnOff() {};
+  }
+
+  public class DishWasher extends HomeAppliancies {
+    public void turnOn() {};
+    public void turnOff() {};
+  }
+}
